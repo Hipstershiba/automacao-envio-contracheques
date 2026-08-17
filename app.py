d83436c4b80274229d967f82f2566a094b040a97
Hipstershiba <vitorrizzato@gmail.com>
2026-08-17 13:56:50 -0300

    checkpoint 26-08-17: itinial steps and configuration class; next: unit tests for configuration class

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
+# IMPORTS
+from rich import print, inspect
+from rich.traceback import install
+install()
 
+from models.config_model import AppConfig
 
 def main():
     pass
-
 
 if __name__ == "__main__":
     main()
