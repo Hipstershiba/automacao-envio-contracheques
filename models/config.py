2628b9347d3ec2215d72411e3714badbcaf193d9
Hipstershiba <vitorrizzato@gmail.com>
2026-08-21 11:12:43 -0300

    feat: implement AppConfig model for YAML-based configuration management

---
+++ models/config.py
@@ -0,0 +1,39 @@
+from dataclasses import dataclass, asdict
+import os
+import yaml
+
+@dataclass
+class AppConfigData:
+    input_path: str = "./documents/input"
+    output_path: str = "./documents/output"
+    api_url: str = "https://api.assina.rbm.digital/api"
+
+class AppConfig:
+    
+    def __init__(self, config_path:str="./config/config.yml"):
+        self.config_path = config_path
+        self.data: AppConfigData = self._load()
+
+    # --- PRIVATE METHODS --- #
+    def _load(self) -> AppConfigData:
+        if not os.path.exists(self.config_path):
+            data = AppConfigData()
+            self.save(data)
+            return data
+
+        with open(self.config_path, 'r', encoding='utf-8') as file:
+            raw = yaml.safe_load(file)
+        
+        try:
+            return AppConfigData(**raw)
+        except Exception as e:
+            raise ValueError(f"Erro ao carregar configurações: {e}")
+
+
+    # --- PUBLIC METHODS --- 
+    def save(self, data: AppConfigData | None = None) -> None:
+        if data is not None:
+            self.data = data
+        os.makedirs(os.path.dirname(self.config_path), exist_ok=True)
+        with open(self.config_path, 'w', encoding='utf-8') as file:
+            yaml.safe_dump(asdict(self.data), file, default_flow_style=False, allow_unicode=True)
