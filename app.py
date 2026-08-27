4f7e004c3082c28e948d22585ad08e46cfff604b
Hipstershiba <vitorrizzato@gmail.com>
2026-08-27 12:28:29 -0300

    feat(app): integrate LogManager into main execution

--- app.py
+++ app.py
@@ -6,12 +6,16 @@ install()
 from models.dimensa_api import DimensaAPI
 from auth.dimensa_auth import obter_token_dimensa
 import models.config
+from utils.log_manager import LogManager
 
 def main():
+    logger = LogManager()
+    logger.start()
     bearer_token = obter_token_dimensa(navegador="edge")
     api_url = "https://api.assina.rbm.digital/api"
     dimensa = DimensaAPI(auth_token=bearer_token, base_url=api_url)
     print(dimensa.get_signatories())
+    logger.stop()
 
 if __name__ == "__main__":
     main()
